b264156d50a8633151cb1f17d7b9ac6c4cdb446c
isnoopy01 <minhtoanbmt2001@gmail.com>
2025-10-17 19:51:29 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const compression = require("compression");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+require("dotenv").config();
+
+//init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//init DB
+require("./database/init.mongodb");
+
+//init routes
+// app.use("/", require("./routes"));
+
+
+//handle errors
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500;
+    return res.status(statusCode).json({
+        status: "error",
+        error: statusCode,
+        message: error.message || "Internal Server Error",
+    });
+});
+
+module.exports = app;
---
+++ src/core/error.response.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const StatusCode = {
+  BADREQUEST: 400,
+  FORBIDDEN: 403,
+  CONFLICT: 409,
+};
+
+const ReasonStatusCode = {
+  BADREQUEST: "Bad Request",
+  FORBIDDEN: "Forbidden",
+  CONFLICT: "Conflict",
+};
+
+const { StatusCodes, ReasonPhrases } = require("../utils/httpStatusCode");
+
+class ErrorResponse extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+class ConflictRequestError extends ErrorResponse {
+  constructor(
+    message = ReasonStatusCode.CONFLICT,
+    statusCode = StatusCodes.CONFLICT
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class BadRequestError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.BAD_REQUEST,
+    statusCode = StatusCodes.BAD_REQUEST
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class ForbiddenError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.FORBIDDEN,
+    statusCode = StatusCodes.FORBIDDEN
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class AuthFailureError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.UNAUTHORIZED,
+    statusCode = StatusCodes.UNAUTHORIZED
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class NotFoundError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.NOT_FOUND,
+    statusCode = StatusCodes.NOT_FOUND
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class RedisResponseError extends ErrorResponse {
+  constructor(
+    message = ReasonPhrases.INTERNAL_SERVER_ERROR,
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    super(message, statusCode);
+  }
+}
+
+module.exports = {
+  ConflictRequestError,
+  BadRequestError,
+  ForbiddenError,
+  AuthFailureError,
+  NotFoundError,
+  RedisResponseError,
+};
